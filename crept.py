dc27ef349eb400aa21b95ad6bb9f5d803fed9e2e
SAMashiyane <salio.blue@gmail.com>
2020-10-16 07:42:25 -0700

    add crept

---
+++ crept.py
@@ -0,0 +1,8 @@
+import os
+filename = input('input_filename plz: ')
+if os.path.isfile(filename) and filename[-3:] == 'exe':
+    str = open(filename,'rb').read()
+    print(str)
+else:
+    print("no dis file found!!!")
+
